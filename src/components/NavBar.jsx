3916f6364a8488d322f08b981fafccbb80df2bd4
Nirmali1234 <niniperera1234@gmail.com>
2020-08-30 11:04:28 +0530

    Updated index.js and NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,11 @@ class NavBar extends Component {
     render() {
       return (
         <nav className="navbar navbar-dark bg-dark">
-          <Link to="/">Home</Link>
-          <Link to="/Index">Products</Link>
+          <Link to="/">Login</Link>
+          <Link to="/Laptop">Laptops</Link>
+          <Link to="/Desktop">Desktops</Link>
+          <Link to="/HotDeals">HotDeals</Link>
+          <Link to="/Accessories">Accessories</Link>
         </nav>
       );
     }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import {BrowserRouter, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
-import Home from "./components/Home";
+import Login from "./components/login";
+import Laptop from "./components/laptopCategory";
+import Desktop from "./components/desktopCategory";
+import HotDeals from "./components/hotDealsCategory";
+import Accessories from "./components/accessoriesCategory";
 
 ReactDOM.render(
     <BrowserRouter>
       <NavBar />
-      <Route exact path="/" component={Home}/>
-    </BrowserRouter>,
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/Laptop" component={Laptop}/>
+      <Route exact path="/Desktop" component={Desktop}/>
+      <Route exact path="/Accessories" component={Accessories}/>
+      <Route exact path="/HotDeals" component={HotDeals}/>
+   </BrowserRouter>,
   document.getElementById('root')
 );
 
