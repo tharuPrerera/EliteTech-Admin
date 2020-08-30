e65c008d0fcb221ff60e7f8c4631010364067a27
Nirmali1234 <niniperera1234@gmail.com>
2020-08-30 13:59:41 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ import Laptop from "./components/laptopCategory";
 import Desktop from "./components/desktopCategory";
 import HotDeals from "./components/hotDealsCategory";
 import Accessories from "./components/accessoriesCategory";
+import UpdateForm from "./components/UpdateProducts/UpdateLaptopItems";
+import AddProduct from "./components/AddProducts/addLaptops";
 
 ReactDOM.render(
     <BrowserRouter>
       <NavBar />
       <Route exact path="/" component={Login}/>
+      <Route exact path="/UpdateProducts/UpdateLaptopItems/:id" component={UpdateForm}/>
+      <Route exact path="/AddProducts/addLaptops" component={AddProduct}/>
       <Route exact path="/Laptop" component={Laptop}/>
       <Route exact path="/Desktop" component={Desktop}/>
       <Route exact path="/Accessories" component={Accessories}/>
